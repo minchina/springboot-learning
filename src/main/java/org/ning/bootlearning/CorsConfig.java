269eec7fb70120be470a07d5c03ad3631c2f1e33
minchina <minchina91@gmail.com>
2017-05-26 00:34:42 +0800

    update spring boot version

---
+++ src/main/java/org/ning/bootlearning/CorsConfig.java
@@ -0,0 +1,21 @@
+package org.ning.bootlearning;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/user").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
+}
--- src/main/java/org/ning/bootlearning/controller/IndexController.java
+++ src/main/java/org/ning/bootlearning/controller/IndexController.java
@@ -5,10 +5,12 @@ import org.ning.bootlearning.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping(value = "/")
 public class IndexController {
 
@@ -18,5 +20,12 @@ public class IndexController {
     public String showIndex(Map<String,Object> model){
         model.put("user", new User("ncmao", 22));
         return "index";
+    }
+
+
+
+    @RequestMapping(value = "/user")
+    public String getToken(@RequestParam String name) {
+        return name;
     }
 }
