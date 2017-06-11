cc85175b5616612f40f8fcad0984fcd9f0568afe
minchina <minchina91@gmail.com>
2017-06-11 23:41:35 +0800

    init mybatis

---
+++ src/main/java/org/ncmao/PersonService.java
@@ -0,0 +1,26 @@
+package org.ncmao;
+
+
+import org.ncmao.domain.Person;
+import org.ncmao.mapper.PersonMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    public void save(Person person) {
+        personMapper.save(person);
+    }
+
+    public Person findById(Long id) {
+        return personMapper.selectById(id);
+    }
+
+    public void deleteAll() {
+        personMapper.deleteAll();
+    }
+}
--- src/test/java/org/ncmao/BoogLearningApplicationTests.java
+++ src/test/java/org/ncmao/BoogLearningApplicationTests.java
@@ -1,16 +1,50 @@
 package org.ncmao;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ncmao.domain.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoogLearningApplicationTests {
 
+	@Autowired
+	private PersonService personService;
+
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void shouldSavePersonSuccess(){
+		Person person = new Person();
+		person.setName("test");
+		person.setAge(11);
+		personService.save(person);
+	}
+
+	@Test
+	public void shouldfindById(){
+		Person person = new Person();
+		person.setName("test111");
+		person.setAge(11);
+
+		Person foundPerson = personService.findById(person.getId());
+		assertThat(foundPerson.getName(), is("test111"));
+	}
+
+	@After
+	public void destory(){
+		personService.deleteAll();
 	}
 
 }
