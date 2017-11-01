501345bc5189760de9f6e50b246012edab09b0a3
ncmao <minchina91@gmail.com>
2017-11-01 23:14:39 +0800

    try something, nothing

--- src/main/java/org/ncmao/controller/SendSmsController.java
+++ src/main/java/org/ncmao/controller/SendSmsController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author ncmao
+ */
 @RestController
 @RequestMapping("/sms")
 public class SendSmsController {
--- src/test/java/org/ncmao/SingletonTest.java
+++ src/test/java/org/ncmao/SingletonTest.java
@@ -1,13 +1,49 @@
 package org.ncmao;
 
+import net.logstash.logback.marker.LogstashMarker;
 import org.junit.Test;
 import org.ncmao.domain.Singleton;
+import org.ncmao.domain.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+
+import static net.logstash.logback.marker.Markers.appendFields;
 
 public class SingletonTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SingletonTest.class);
     @Test
     public void canOnlyGetOneInstance(){
         Singleton singleton = Singleton.getInstance();
         System.out.println(singleton);
     }
+
+
+    @Test
+    public void shouldTest1() throws IllegalAccessException {
+        Student student = new Student();
+        student.setName("test");
+        student.setAge(11);
+        mapAll(student);
+    }
+
+
+
+    private void mapAll(Student student) throws IllegalAccessException {
+        Class studentClass = student.getClass();
+        Field[] declaredFields = studentClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(declaredField.getName());
+            declaredField.setAccessible(true);
+            System.out.println(declaredField.get(student));
+        }
+        LogstashMarker logstashMarker = appendFields(student);
+        LOGGER.info(logstashMarker, "test");
+
+
+    }
+
+
 }
